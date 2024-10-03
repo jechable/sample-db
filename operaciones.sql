459db7c7ab05d43447646b4c907ce663a4204020
Julián E. Chablé <39680872+jechable@users.noreply.github.com>
2024-10-03 16:31:30 -0600

    Update operaciones.sql

--- operaciones.sql
+++ operaciones.sql
@@ -4,11 +4,15 @@
 INSERT INTO Clientes (nombre, email, telefono, direccion)
 VALUES ('Juan Lopez', 'juan@gmail.com', '996 101 8694', 'Calle 13 entre Reforma y Zanzibar');
 
-INSERT INTO Tecnico (nombre_tecnico, email, especialidad)
+INSERT INTO Tecnicos (nombre_tecnico, email, especialidad)
 VALUES ('Luis Pech', 'lc2453@gmail.com', 'mantenimiento');
 
 INSERT INTO Impresoras (modelo_impresora, tipo_impresora, estado_impresora)
 VALUES ('Proteus 9000', 'Multifuncional', 'Operativa');
+
+INSERT INTO Impresoras (modelo_impresora, tipo_impresora, estado_impresora)
+VALUES ('Thinker Pro', 'Multifuncional', 'Descompuesta');
+
 
 -- Actualizaciones
 UPDATE Clientes SET nombre = 'Carlos Gonzalez' 
@@ -22,12 +26,10 @@ WHERE id_producto = 3;
 
 -- Borrado
 DELETE FROM Clientes
-WHERE id = 1;
+WHERE id_cliente = 6;
 
 DELETE FROM Impresoras
-WHERE modelo_impresora = 'Anycubic Photon';
+WHERE modelo_impresora = 'Thinker Pro';
 
 DELETE FROM Pedidos
-WHERE id = 1;
-
-
+WHERE id_pedido = 1;
